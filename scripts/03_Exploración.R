0da04eca454b11c1f4c0363d0abdb771510871b2
elianeesvida <a20213997@pucp.edu.pe>
2026-07-05 11:39:20 -0500

    Preparo las etiquetas

--- scripts/03_Exploración.R
+++ scripts/03_Exploración.R
@@ -24,4 +24,50 @@ renv::snapshot()
 
 enaho_mascotas <- read_parquet(here("datos", "procesados", "enaho_mascotas_acondicionada_030726.parquet"))
 
+# ------------------------------------------------------------------------------
+# 1. PREPARACIÓN DE ETIQUETAS--------------------------------------------------
+# ------------------------------------------------------------------------------
+enaho_explorar <- enaho_mascotas %>%
+  mutate(
+    # A. Tenencia de mascotas
+    tiene_mascota_etiqueta = factor(tiene_mascota,
+                                    levels = c(FALSE, TRUE),
+                                    labels = c("Sin mascota", "Con mascota")),
+    tiene_perro_etiqueta = factor(tiene_perro,
+                                  levels = c(FALSE, TRUE),
+                                  labels = c("Sin perro", "Con perro")),
+    tiene_gato_etiqueta = factor(tiene_gato,
+                                 levels = c(FALSE, TRUE),
+                                 labels = c("Sin gato", "Con gato")),
+    tiene_otra_mascota_etiqueta = factor(tiene_otra_mascota,
+                                         levels = c(FALSE, TRUE),
+                                         labels = c("Sin otra mascota", "Con otra mascota")),
+    
+    # B. Condiciones de vivienda - NBI
+    nbi1_etiqueta = factor(nbi1, levels = c(0, 1),
+                           labels = c("Vivienda adecuada", "Vivienda inadecuada")),
+    nbi2_etiqueta = factor(nbi2, levels = c(0, 1),
+                           labels = c("Sin hacinamiento", "Con hacinamiento")),
+    nbi3_etiqueta = factor(nbi3, levels = c(0, 1),
+                           labels = c("Con servicios higiénicos", "Sin servicios higiénicos")),
+    
+    # C. Área geográfica
+    # Estratos 1-4: Lima Metropolitana, Costa urbana, Sierra urbana, Selva urbana
+    # Estratos 5-8: Costa rural, Sierra rural, Selva rural, Lima rural
+    area = case_when(
+      estrato %in% c(1, 2, 3, 4, 5,6) ~ "Urbano",
+      estrato %in% c(7, 8) ~ "Rural",
+      TRUE                        ~ NA_character_
+    ),
+    area = factor(area, levels = c("Urbano", "Rural")),
+    
+    # D. Limpieza numérica
+    estrato  = as.numeric(estrato),
+    conglome = as.numeric(conglome),
+    factor_s = as.numeric(factor_s)
+  )
+
+write_parquet(enaho_explorar, "datos/procesados/enaho_mascotas_explorar_030726.parquet")
+
+
 
